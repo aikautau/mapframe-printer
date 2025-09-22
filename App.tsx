c3c17b70fbd8475d75d9b41a287979f793ccf5d9
aikautau <github-noreply@github.com>
2025-09-22 11:00:20 +0900

    Change map style to Stamen Toner Lite for text-free minimal design

--- App.tsx
+++ App.tsx
@@ -58,11 +58,11 @@ const App: React.FC = () => {
         zoomSnap: 0, // Enable fractional zoom
       });
 
-      window.L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+      window.L.tileLayer('https://stamen-tiles-{s}.a.ssl.fastly.net/toner-lite/{z}/{x}/{y}{r}.png', {
+        attribution: 'Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
         detectRetina: true,
-        maxZoom: 19,
-        maxNativeZoom: 19,
+        maxZoom: 20,
+        maxNativeZoom: 20,
       }).addTo(map);
 
       window.L.control.zoom({ position: 'bottomright' }).addTo(map);
